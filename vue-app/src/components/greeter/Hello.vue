d3a840e533cde988a27663a82b55104d4928d98c
Subramanian Murugan <sasubramanian_md@hotmail.com>
2023-07-18 12:33:28 +0530

    vue component composition

---
+++ vue-app/src/components/greeter/Hello.vue
@@ -0,0 +1,39 @@
+<!-- options api pattern -->
+
+<!-- <script>
+import Message from './Message.vue';
+
+export default {
+    components: { Message }
+}
+</script>
+<template>
+    <Message></Message>
+</template>
+<style></style> -->
+
+<!-- Composition api : using setup function -->
+
+<!-- <script>
+import Message from './Message.vue';
+
+export default {
+    components: { Message },
+    setup() {
+
+    }
+}
+</script>
+<template>
+    <Message></Message>
+</template>
+<style></style> -->
+
+<!-- Composition api : using setup attribute -->
+<script setup>
+import Message from './Message.vue';
+</script>
+<template>
+    <Message></Message>
+</template>
+<style></style>
---
+++ vue-app/src/components/greeter/Message.vue
@@ -0,0 +1,30 @@
+<!-- options api pattern -->
+<!-- <script>
+export default {
+
+}
+</script>
+<template>
+    <h1>Hello</h1>
+</template>
+<style></style> -->
+<!-- composition  api pattern : using setup function -->
+<!-- <script>
+export default {
+    setup() {
+
+    }
+}
+</script>
+<template>
+    <h1>Hello</h1>
+</template>
+<style></style>  -->
+
+<!-- composition  api pattern : using setup attribute -->
+<script setup>
+</script>
+<template>
+    <h1>Hello</h1>
+</template>
+<style></style> 
